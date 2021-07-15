fa52741904ea9496f46edf2d31791b0dd5bcdbff
Chris Oates <chrispoates@gmail.com>
2021-07-14 20:44:50 -0400

    First commit. Basic functionality works, shows adj lists, but no algorithm results.

---
+++ graph_algs.js
@@ -0,0 +1,15 @@
+// takes two adjacency lists (arrays of arrays). This function assumes these are undirected graphs
+function isomorphic(g1, g2) {
+    if (g1.length !== g2.length) {
+      return false;
+    }
+    // first, cycle through all possible mappings
+    for (let i=0; i<g1.length; i++) {
+      
+    }
+    return true;
+}
+
+function permute(nodes) {
+    let s = "";
+}
