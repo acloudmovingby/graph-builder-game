c59cbf6ad28b6b17fe78b1aa2fd0fb153bd551f3
Chris Oates <chrispoates@gmail.com>
2021-07-14 22:48:54 -0400

    Currently working on graph_algs

---
+++ public/canvas.js
@@ -0,0 +1,239 @@
+let canvas = document.getElementById("canvas");
+const infoPaneWidth = 400; // this MUST match the width of the info-pane set out in css
+let nodes = [];
+let edgeMode = false;
+let edgeStart = null;
+let edgeCount = 0;
+let mouseX = 0;
+let mouseY = 0;
+let nodeHover = null;
+let stillInNode = false; // true if mouse is still inside node bounds for a node that was just created. helps it so the hover effect doesn't happen immediately after adding node
+const messageState = {
+  message: "",
+  display: false,
+  startTime: 0,
+};
+
+const timeInit = new Date().getSeconds();
+const nodeRadius = 15;
+
+if (canvas.getContext) {
+  canvas.addEventListener("mousedown", canvasClick, false);
+  canvas.addEventListener("mousemove", mouseMove, false);
+  canvas.addEventListener("mouseleave", mouseLeave, false);
+  window.requestAnimationFrame(draw);
+}
+
+function canvasClick(event) {
+  let canvasBounds = canvas.getBoundingClientRect();
+  let x = event.x - canvasBounds.left;
+  let y = event.y - canvasBounds.top;
+
+  let nodeClicked = nodeAtPoint(x, y, nodes);
+
+  if (!edgeMode && !nodeClicked) {
+    // create new Node
+    nodes.push(new Node(nodes.length, 0, x, y));
+    let node = document.createElement("LI");
+    node.appendChild(document.createTextNode(nodes.length + ": "));
+    document.getElementById("adjacency-list").appendChild(node);
+    stillInNode = true;
+    document.getElementById("node-count").innerHTML = nodes.length;
+  } else if (!edgeMode) {
+    // start edge on the node clicked
+    edgeMode = true;
+    edgeStart = nodeClicked;
+  } else if (nodeClicked && nodeClicked != edgeStart) {
+    if (!edgeStart.neighbors.includes(nodeClicked)) {
+      edgeStart.neighbors.push(nodeClicked);
+      nodeClicked.neighbors.push(edgeStart);
+      // update adjacency list on webpage
+      let adjList = document.getElementById("adjacency-list");
+      if (adjList.hasChildNodes()) {
+        let items = adjList.childNodes;
+        let startIx = 0;
+        let clickedIx = 0;
+        for (let i = 0; i < nodes.length; i++) {
+          if (nodes[i] === edgeStart) {
+            startIx = i;
+          }
+          if (nodes[i] === nodeClicked) {
+            clickedIx = i;
+          }
+        }
+        edgeStart = nodeClicked;
+        edgeCount++;
+        document.getElementById("edge-count").innerHTML = edgeCount;
+        items[startIx].appendChild(document.createTextNode(" " + clickedIx));
+        items[clickedIx].appendChild(document.createTextNode(" " + startIx));
+      }
+    }
+    edgeStart = nodeClicked;
+  } else {
+    // cancel edge mode
+    edgeMode = false;
+    edgeStart = null;
+  }
+}
+
+function draw() {
+  if (canvas.getContext) {
+    let ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth - infoPaneWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, window.innerWidth * 2, window.innerHeight * 2);
+
+    // start message
+    if (nodes.length === 0) {
+      ctx.font = "25px Arial";
+      ctx.fillStyle = "gray";
+      ctx.textAlign = "center";
+      ctx.fillText(
+        "Welcome! To start, try clicking somewhere",
+        ctx.canvas.width / 2,
+        ctx.canvas.height / 2
+      );
+    }
+
+    //draw temp edge
+    if (edgeMode) {
+      ctx.lineWidth = 8;
+      ctx.strokeStyle = "#ffdc7a";
+      ctx.moveTo(edgeStart.x, edgeStart.y);
+      ctx.lineTo(mouseX, mouseY);
+      ctx.closePath();
+      ctx.stroke();
+    }
+
+    // draw edges
+    let edges = getEdges(nodes);
+    edges.forEach((e) => {
+      ctx.beginPath();
+      ctx.lineWidth = 8;
+      ctx.strokeStyle = "orange";
+      ctx.moveTo(e[0].x, e[0].y);
+      ctx.lineTo(e[1].x, e[1].y);
+      ctx.closePath();
+      ctx.stroke();
+    });
+
+    // draw nodes
+    for (let i = 0; i < nodes.length; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 8;
+      if (nodes[i] === edgeStart) {
+        ctx.strokeStyle = "#FA5750";
+        ctx.fillStyle = "#FA5750";
+      } else if (edgeMode) {
+        ctx.strokeStyle = "#FA5750";
+        ctx.fillStyle = "white";
+      } else {
+        ctx.strokeStyle = "#32BFE3";
+        ctx.fillStyle = "#32BFE3";
+      }
+
+      let oscillator = Math.cos(nodes[i].counter / 2 + 8); // oscillates -1.0 to 1.0
+      let dampener = Math.min(1, 1 / (nodes[i].counter / 2)) + 0.05;
+      let dampener2 = Math.min(1, 1 / (nodes[i].counter / 10));
+      let radius = Math.max(
+        1,
+        25 * oscillator * dampener * dampener2 + nodeRadius
+      );
+      ctx.arc(nodes[i].x, nodes[i].y, radius, 0, Math.PI * 2, false);
+      ctx.stroke();
+      ctx.fill();
+
+      // hover effects
+      if (nodes[i] === nodeHover && !stillInNode) {
+        ctx.closePath();
+        ctx.beginPath();
+        if (!edgeMode) {
+          ctx.lineWidth = 4;
+          ctx.arc(nodes[i].x, nodes[i].y, radius + 10, 0, Math.PI * 2, false);
+          ctx.stroke();
+        } else {
+          ctx.fillStyle = "#FA5750";
+          ctx.arc(nodes[i].x, nodes[i].y, radius - 4, 0, Math.PI * 2, false);
+          ctx.fill();
+        }
+      }
+      // prevent overflow, don't increment indefinitely
+      if (nodes[i].counter < 1000) {
+        nodes[i].counter += 1;
+      }
+    }
+    // message box text
+    /*
+    let boxX = (ctx.canvas.width - infoPaneWidth) / 2;
+    let boxY = ctx.canvas.height - 100;
+    let boxWidth = 300;
+    let boxHeight = 35;
+    ctx.fillStyle = "white";
+    ctx.fillRect(
+      boxX,
+      boxY,
+      boxWidth,
+      boxHeight
+    );
+    ctx.font = "15px Arial";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText(
+      "Not allowed",
+      boxX + boxWidth/2,
+      boxY + boxHeight/2
+    );*/
+  }
+  window.requestAnimationFrame(draw);
+}
+
+function Node(index, counter, x, y) {
+  this.index = index;
+  this.counter = counter;
+  this.x = x;
+  this.y = y;
+  this.neighbors = [];
+}
+
+// returns the node, if any, located at those coordinates. Assumes coordinates are relative to canvas, not window.
+function nodeAtPoint(x, y, nodes) {
+  for (let i = 0; i < nodes.length; i++) {
+    let dx = x - nodes[i].x;
+    let dy = y - nodes[i].y;
+    let distFromCent = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    if (distFromCent < nodeRadius * 2) {
+      return nodes[i];
+    }
+  }
+  return null;
+}
+
+function mouseLeave(event) {
+  edgeMode = false;
+  edgeStart = null;
+}
+
+function mouseMove(event) {
+  let canvasBounds = canvas.getBoundingClientRect();
+  mouseX = event.x - canvasBounds.left;
+  mouseY = event.y - canvasBounds.top;
+
+  nodeHover = nodeAtPoint(mouseX, mouseY, nodes);
+  if (!nodeHover) {
+    stillInNode = false;
+  }
+}
+
+function getEdges(nodes) {
+  let edges = [];
+  let marked = new Set();
+  for (let i = 0; i < nodes.length; i++) {
+    marked.add(nodes[i]);
+    for (let j = 0; j < nodes[i].neighbors.length; j++) {
+      if (!marked.has(nodes[i].neighbors[j])) {
+        edges.push([nodes[i], nodes[i].neighbors[j]]);
+      }
+    }
+  }
+  return edges;
+}
---
+++ public/graph_algs.js
@@ -0,0 +1,50 @@
+let graph1 = [[]];
+let graph2 = [[]];
+let map = new Map();
+let result = checkMapping(graph1, graph2, map);
+console.log(`checkMapping: ${result}`);
+
+function checkMapping(g1, g2, map) {
+  let mapCorrect = true;
+  let edges = getEdges(g1);
+  edges.forEach((e) => {
+    // get equivalents in g2
+    // check that the first node in g2 has the other node as an edge
+    let g2_source = map.get(e[0]);
+    let g2_target = map.get(e[1]);
+    mapCorrect += g2[g2_source].includes(g2_target);
+  });
+  return mapCorrect;
+}
+
+// takes two adjacency lists (arrays of arrays). This function assumes these are undirected graphs
+function isomorphic(g1, g2) {
+  let isomorphic = bruteForce(g1, g2);
+
+  if (g1.length !== g2.length) {
+    return false;
+  }
+  // first, cycle through all possible mappings
+  for (let i = 0; i < g1.length; i++) {
+    for (let j = 0; j < g2.length; j++) {}
+  }
+  return true;
+}
+
+function permute(nodes) {
+  let s = "";
+}
+
+function getEdges(nodes) {
+  let edges = [];
+  let marked = new Set();
+  for (let i = 0; i < nodes.length; i++) {
+    marked.add(nodes[i]);
+    for (let j = 0; j < nodes[i].length; j++) {
+      if (!marked.has(nodes[i][j])) {
+        edges.push(i, j);
+      }
+    }
+  }
+  return edges;
+}
