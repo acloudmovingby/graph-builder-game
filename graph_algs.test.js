2e920de5118ba85b9eaf04dd718898214637fa05
Chris Oates <chrispoates@gmail.com>
2021-07-19 14:49:44 -0400

    Now all isomorphism tests use the import correctly. Two tests don't pass and the algorithm is not working yet.

--- graph_algs.test.js
+++ graph_algs.test.js
@@ -1,4 +1,4 @@
-const { checkEdges } = require("./public/graph_algs");
+const { checkEdges, isomorphism } = require("./public/graph_algs");
 
 test("checks edges with empty graphs", () => {
   expect(checkEdges([], [], [])).toBe(true);
@@ -140,58 +140,58 @@ test("checks graphs with four nodes and four edges", () => {
   perm = [0, 1, 2, 3];
   expect(checkEdges(perm, g1, g2)).toBe(false);
 });
-/*
+
 test("isomorphism - on empty graphs should be true (they're both empty sets)", () => {
-    let g1 = [];
-    let g2 = [];
-    expect(isomorphism(g1,g2).toBe(true))
+  let g1 = [];
+  let g2 = [];
+  expect(isomorphism(g1, g2)).toBe(true);
 });
 
 test("isomorphism - simple fail with one node/zero node; commutative", () => {
-    let g1 = [[]];
-    let g2 = [];
-    expect(isomorphism(g1,g2).toBe(false));
-    expect(isomorphism(g2,g1).toBe(false));
+  let g1 = [[]];
+  let g2 = [];
+  expect(isomorphism(g1, g2)).toBe(false);
+  expect(isomorphism(g2, g1)).toBe(false);
 });
 
 test("isomorphism - one node in each graph", () => {
-    let g1 = [[]];
-    let g2 = [[]];
-    expect(isomorphism(g1,g2).toBe(true))
-    expect(isomorphism(g2,g1).toBe(true))
+  let g1 = [[]];
+  let g2 = [[]];
+  expect(isomorphism(g1, g2)).toBe(true);
+  expect(isomorphism(g2, g1)).toBe(true);
 });
 
 test("isomorphism - 2/0 nodes, commutative ", () => {
-    let g1 = [[],[]];
-    let g2 = [];
-    expect(isomorphism(g1,g2).toBe(false));
-    expect(isomorphism(g2,g1).toBe(false));
+  let g1 = [[], []];
+  let g2 = [];
+  expect(isomorphism(g1, g2)).toBe(false);
+  expect(isomorphism(g2, g1)).toBe(false);
 });
 
 test("isomorphism - 2/1 nodes, commutative ", () => {
-    let g1 = [[],[]];
-    let g2 = [[]];
-    expect(isomorphism(g1,g2).toBe(false));
-    expect(isomorphism(g2,g1).toBe(false));
+  let g1 = [[], []];
+  let g2 = [[]];
+  expect(isomorphism(g1, g2)).toBe(false);
+  expect(isomorphism(g2, g1)).toBe(false);
 });
 
 test("isomorphism - test with isomorphic 2/2 nodes, 1/1 edges; commutative", () => {
-    let g1 = [[1],[0]];
-    let g2 = [[1],[0]];
-    expect(isomorphism(g1,g2).toBe(true));
-    expect(isomorphism(g2,g1).toBe(true));
+  let g1 = [[1], [0]];
+  let g2 = [[1], [0]];
+  expect(isomorphism(g1, g2)).toBe(true);
+  expect(isomorphism(g2, g1)).toBe(true);
 });
 
 test("isomorphism - test with non-isomorphic 2/2 nodes, 1/0 edges; commutative", () => {
-    let g1 = [[1],[0]];
-    let g2 = [[],[]];
-    expect(isomorphism(g1,g2).toBe(false));
-    expect(isomorphism(g2,g1).toBe(false));
+  let g1 = [[1], [0]];
+  let g2 = [[], []];
+  expect(isomorphism(g1, g2)).toBe(false);
+  expect(isomorphism(g2, g1)).toBe(false);
 });
 
 test("isomorphism - test with non-isomorphic 2/2 nodes, 1/0 edges; commutative", () => {
-    let g1 = [[1],[0]];
-    let g2 = [[],[]];
-    expect(isomorphism(g1,g2).toBe(false));
-    expect(isomorphism(g2,g1).toBe(false));
-});*/
+  let g1 = [[1], [0]];
+  let g2 = [[], []];
+  expect(isomorphism(g1, g2)).toBe(false);
+  expect(isomorphism(g2, g1)).toBe(false);
+});
