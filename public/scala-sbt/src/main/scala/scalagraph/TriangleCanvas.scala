9f521ff0b0e2b678492ce6823ae74daf57c01484
Chris Oates <chrispoates@gmail.com>
2025-09-22 14:41:09 +0100

    Started adding case class for triangles to draw on the HTML Canvas.

---
+++ public/scala-sbt/src/main/scala/scalagraph/TriangleCanvas.scala
@@ -0,0 +1,27 @@
+package scalagraph
+
+import scala.scalajs.js
+
+import scalagraph.{Point, PointJS}
+
+case class TriangleCanvas(
+	 pt1: Point,
+	 pt2: Point,
+	 pt3: Point,
+	 color: String // Hex string, e.g. "#FF0000"
+) {
+	def toJS: TriangleCanvasJS = js.Dynamic.literal(
+		pt1 = this.pt1.toJS,
+		pt2 = this.pt2.toJS,
+		pt3 = this.pt3.toJS,
+		color = this.color
+	).asInstanceOf[TriangleCanvasJS]
+}
+
+@js.native
+trait TriangleCanvasJS extends js.Object {
+	val pt1: PointJS
+	val pt2: PointJS
+	val pt3: PointJS
+	val color: String
+}
