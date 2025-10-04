72e5bed8e7ce23ba7d32f94d20fe62baf22b5d85
Chris Oates <chrispoates@gmail.com>
2025-10-04 17:11:28 -0400

    Made tests actually test something meaningful about the GraphController. This makes me definitely realize though that I want a facade to the GraphController that simply converts things to/from JS classes so I can make these unit tests more purely Scala.

--- public/scala/graphcontroller/test/src/GraphControllerTests.scala
+++ public/scala/graphcontroller/test/src/GraphControllerTests.scala
@@ -2,11 +2,21 @@ package graphcontroller
 
 import utest.*
 
+import graphcontroller.GraphController
+import graphcontroller.dataobject.NodeData
+
 object GraphControllerTests extends TestSuite {
 	def tests = Tests {
-		test("hello") {
-			assert(1 == 1)
-			1
+		test("Test clearing the graph") {
+			val controller = new GraphController()
+			controller.addNode(1, NodeData(0, 0, 0).toJS)
+			controller.addNode(2, NodeData(0, 0, 0).toJS)
+			controller.addEdge(1, 2)
+			assert(controller.nodeCount() == 2)
+			assert(controller.edgeCount() == 1)
+			controller.clearGraph()
+			assert(controller.nodeCount() == 0)
+			assert(controller.edgeCount() == 0)
 		}
 	}
 }
