ac9891628daeb9942724a10e2aaafd13dadc1099
Chris Oates <30088765+acloudmovingby@users.noreply.github.com>
2026-03-29 18:59:15 -0400

    Started adding select tool logic including a feature flag to toggle. (#44)

--- public/scalajs/graphcontroller/src/graphcontroller/components/toolbar/ToolBarComponent.scala
+++ public/scalajs/graphcontroller/src/graphcontroller/components/toolbar/ToolBarComponent.scala
@@ -3,13 +3,14 @@ package graphcontroller.components.toolbar
 import graphcontroller.components.{Component, RenderOp}
 import graphcontroller.controller.{Event, ToolBarMouseOut, ToolBarMouseOver, ToolSelected}
 import graphcontroller.model.State
-import graphcontroller.shared.{AreaCompleteTool, BasicTool, MagicPathTool, MoveTool}
+import graphcontroller.shared.{AreaCompleteTool, BasicTool, MagicPathTool, MoveTool, SelectTool}
 
 object ToolBarComponent extends Component {
 	override def update(state: State, event: Event): State = {
 		event match {
 			case ToolSelected(tool) =>
 				val newTool = tool match {
+					case "select" => SelectTool()
 					case "basic" => BasicTool(None)
 					case "area-complete" => AreaCompleteTool(false, Nil)
 					case "magic-path" => MagicPathTool(None)
@@ -27,6 +28,7 @@ object ToolBarComponent extends Component {
 
 	override def view(state: State): RenderOp = ToolBarRenderData(
 		selectedTool = state.toolState,
-		hoveringOnTool = state.hoveringOnTool
+		hoveringOnTool = state.hoveringOnTool,
+		state.featureFlags.selectTool
 	)
 }
