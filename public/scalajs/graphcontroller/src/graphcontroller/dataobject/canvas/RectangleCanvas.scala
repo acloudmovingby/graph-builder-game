b2151722e38220c6fbc11919569c6e67cdcafa96
Chris Oates <chrispoates@gmail.com>
2026-03-31 23:04:42 -0400

    Added border color option to RectangleCanvas and renamed the existing color parameter to specify that it is the fill color not stroke color.

--- public/scalajs/graphcontroller/src/graphcontroller/dataobject/canvas/RectangleCanvas.scala
+++ public/scalajs/graphcontroller/src/graphcontroller/dataobject/canvas/RectangleCanvas.scala
@@ -7,15 +7,21 @@ import org.scalajs.dom
 /** Represents data necessary to draw a triangle with the HTML Canvas API */
 case class RectangleCanvas(
 	rect: Rectangle,
-	color: String // Hex string, e.g. "#FF0000"
+	fillColor: String, // Hex string, e.g. "#FF0000"
+	borderColor: Option[String] = None
 ) extends CanvasRenderOp, Shape {
 	type This = RectangleCanvas
 
 	def draw(ctx: dom.CanvasRenderingContext2D): Unit = {
 		ctx.beginPath()
 		ctx.rect(rect.topLeft.x, rect.topLeft.y, rect.width, rect.height)
-		ctx.fillStyle = color
+		ctx.fillStyle = fillColor
 		ctx.fill()
+		borderColor.foreach { color =>
+			ctx.strokeStyle = color
+			ctx.lineWidth = 1.0
+			ctx.stroke()
+		}
 	}
 
 	def translate(vec: Vector2D): RectangleCanvas = this.copy(
